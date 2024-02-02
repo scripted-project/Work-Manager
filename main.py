0c2c704ad9553554acc2e4921be20c3175a29404
Jewels <107947731+Jewels333@users.noreply.github.com>
2024-02-02 16:03:36 +0000

    trying to fix css

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import logging
 from modules.logs import Logger
 from modules.site import Site
 from modules.api import API
+from modules.encryption import new_key
 
 app = Flask(__name__)
+app.secret_key = new_key(1024)
 
 unusedLog = logging.getLogger('werkzeug')
 unusedLog.setLevel(logging.ERROR)
@@ -23,3 +25,4 @@ api = API(app, logger, shared)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host='0.0.0.0')
+    

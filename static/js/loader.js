2bd41f4f58271a72939ec0f95501d6280cb99096
marked9999 <jewelsytofficial@gmail.com>
2024-02-07 16:48:58 +0000

    fixing iframes

--- static/js/loader.js
+++ static/js/loader.js
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 async function load(container, name) {
     const path = `./widgets/${name}/entry.html`;
     if (fs.existsSync(path)) {
-        document.getElementById(container).outerHTML += `<iframe src="./widgets/${name}/entry.html"`
+        document.getElementById(container).outerHTML += `<iframe src="./widgets/${name}/entry.html"></iframe>`
     }
     else {document.getElementById(container).outerHTML = '<h3>not found</h3>'}   
 }

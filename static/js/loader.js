42e808295431e279220bffffc2b1e48beb43ed65
Jewels <107947731+Jewels333@users.noreply.github.com>
2024-02-07 16:14:39 +0000

    iframe -> load widgets

--- static/js/loader.js
+++ static/js/loader.js
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 async function load(container, name) {
     const path = `./widgets/${name}/entry.html`;
     if (fs.existsSync(path)) {
-        document.getElementById(container).innerHTML = fs.readFile(path);
+        document.getElementById(container).innerHTML += `<iframe src="./widgets/${name}/entry.html"`
     }
     
 }

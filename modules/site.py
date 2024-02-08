967135921f69a0908bb7edbf155a4aa6f2d141ce
Jewels <107947731+Jewels333@users.noreply.github.com>
2024-02-08 19:21:46 +0000

    pie-chart check

--- modules/site.py
+++ modules/site.py
@@ -1,6 +1,6 @@
 from modules.logs import Logger
 from modules.server import Server
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
 
 class Site:
     def __init__(self, app: Flask, logger: Logger, server: Server):
@@ -13,18 +13,24 @@ class Site:
             logger.log(f"GET '/': index.html ({response.status_code})")
             return response
         
-        @app.route("/dash")
+        @app.route("/dashboard")
         def dashboard():
             response = make_response(render_template("dashboard.html"))
             logger.log(f"GET '/dash': dashboard.html ({response.status_code})")
             return response
         
-        @app.route("/users/<user>")
-        def dashboard_withID(username: str):
-            response = make_response(render_template("dashboard.html", username=username))
-            logger.log(f"GET '/dash/{username}: dashboard.html [id = {username}] ({response.status_code})")
-            return response
-        
+        @app.route("/login-signup")
+        def login_signup():
+            if request.method == 'POST': 
+                #when the form is submitted
+                username = request.form.get("username", None)
+                password = request.form.get("password", None)
+            elif request.method == 'GET':
+                #when page needs to be rendered
+                response = make_response(render_template("login-signup.html"))
+                logger.log(f"GET '/login-signup': login-signup.html ({response.status_code})")
+                return response
+
         @app.route("/test")
         def test():
             return render_template('test.html')

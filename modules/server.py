2997e259cb6931f9aea4b944da280f8120bcc925
marked9999 <jewelsytofficial@gmail.com>
2024-02-21 17:04:44 +0000

    0.5.2.6

--- modules/server.py
+++ modules/server.py
@@ -16,9 +16,9 @@ class Server:
 
     def newDash(self, user: str):
         self.clients[user]["dashboards"].append(self._nextDashboardID)
-        f = open(f"/data/dashboards/{self._nextDashboardID}.json", 'x')
+        f = open(f"data/dashboards/{self._nextDashboardID}.json", 'x')
         f.write("{}")
-        json = JSONFile(f'/data/dashboards/{self._nextDashboardID}.json')
+        json = JSONFile(f'data/dashboards/{self._nextDashboardID}.json')
         json.data = {"id": self._nextDashboardID, "widgets": []}
         json.save()
         self._nextDashboardID += 1
